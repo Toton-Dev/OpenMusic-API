fab0822e5b198183e1cd7a85e42d0dcaf88d3f06
Toton Dwi Antoko <Totonantoko@gmail.com>
2021-08-10 07:59:15 +0700

    remove user from collaboration

--- src/api/collaborations/handler.js
+++ src/api/collaborations/handler.js
@@ -5,7 +5,7 @@ class CollaborationsHandler {
     this._validator = validator;
 
     this.postCollaborationHandler = this.postCollaborationHandler.bind(this);
-    // this.deleteCollaborationHandler = this.deleteCollaborationHandler.bind(this);
+    this.deleteCollaborationHandler = this.deleteCollaborationHandler.bind(this);
   }
 
   async postCollaborationHandler(request, h) {
@@ -32,6 +32,28 @@ class CollaborationsHandler {
       response.code(201);
       return response;
     } catch (error) {
+      return error;
+    }
+  }
+
+  async deleteCollaborationHandler(request) {
+    try {
+      // this._validator.validateCollaborationPayload(request.payload);
+      const { id: credentialId } = request.auth.credentials;
+      const { playlistId, userId } = request.payload;
+
+      // await this._playlistsService.verifyPlaylistOwner(playlistId, credentialId);
+      await this._collaborationsService.deleteCollaboration(
+        playlistId,
+        userId,
+      );
+
+      return {
+        status: 'success',
+        message: 'Kolaborasi berhasil dihapus',
+      };
+    } catch (error) {
+      console.log(error);
       return error;
     }
   }
